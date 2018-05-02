3ff3aa4a4e72b944c515684d0832ec314fe38273
IndrajitKlog <indrajit.naiya@kloctechnologies.co.in>
2018-05-02 15:11:15 +0530

    scope bug fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 250 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
+    @low_stock = ShopifyAPI::Product.where(title: "35 Reasons Why I Love You Notebook")
+    @admin_email = ShopifyAPI::Shop.current.email
+    #@low_stock = ShopifyAPI::Product.all
+    #@total = ShopifyAPI::Product.where(variants.barcode: "klog123"})
+    #binding.pry
   end
 end
